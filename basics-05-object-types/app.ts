a2f6c513f0d961632e6b9f908f8c548e4bc41d5f
bsena-it <bsena-it@aidhub.org>
2022-10-20 21:42:58 +0800

    mastering ts

---
+++ basics-05-object-types/app.ts
@@ -0,0 +1,10 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: 'Bima',
+  age: 30
+};
+
+console.log(person.name);
---
+++ config-02-excluding/app.ts
@@ -0,0 +1,3 @@
+const userName = 'Bima';
+
+console.log(userName);
